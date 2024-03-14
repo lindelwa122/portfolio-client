d06e2b519eae44e911ee6ea598496af206fe19b8
Lindelwa <lindelwa.codes@gmail.com>
2024-03-14 21:12:21 +0200

    Add function to handle sending data to server.

--- src/utils/send-data-to-server.jsx
+++ src/utils/send-data-to-server.jsx
@@ -14,4 +14,24 @@ const createDraft = async () => {
   return blog_id;
 }
 
-export { createDraft };
+const saveDataToServer = async (url, data) => {
+  const response = await fetch(serverURI + url, {
+    headers: { 
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data),
+    method: 'POST',
+  });
+
+  if (!response.ok) {
+    const err = new Error(
+      `Something wrong occurred! The status code is ${response.status}`
+    );
+    err.status = response.status;
+    throw err;
+  }
+
+  return { status: response.status, success: true };
+}
+
+export { createDraft, saveDataToServer };
