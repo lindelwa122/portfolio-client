942f352c7ad41e04e7c7d8ab6d09cc8c5a757d7d
Lindelwa <lindelwa.codes@gmail.com>
2024-03-14 12:17:10 +0200

    Add create-blog page structure.

--- src/components/create_blog/create-blog.jsx
+++ src/components/create_blog/create-blog.jsx
@@ -1,5 +1,22 @@
-const CreateBlog = () => {
+import { Outlet }from  'react-router-dom';
+ import Navbar from '../navbar/navbar';
+import Sidebar from '../sidebar/sidebar';
+import styles from './create-blog.module.css';
 
+const CreateBlog = () => {
+  return (
+    <>
+      <Navbar />
+      <main className={styles.main}>
+        <div className={styles.sidebar}>
+          <Sidebar />
+        </div>
+        <div>
+          <Outlet />
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default CreateBlog;
