cf3e707033c21951127c5e424ebe0d0ea5c9f0d6
Lindelwa <lindelwa.codes@gmail.com>
2024-03-14 15:19:54 +0200

    Add click handler.

--- src/components/create_blog_index/create-blog-index.jsx
+++ src/components/create_blog_index/create-blog-index.jsx
@@ -1,9 +1,20 @@
 import styles from './create-blog-index.module.css';
+import { createDraft } from '../../utils/send-data-to-server';
+import { useNavigate } from 'react-router-dom';
 
 const CreateBlogIndex = () => {
+  const navigate = useNavigate();
+
+  const handleClick = async () => {
+    const id = await createDraft().catch(err => {
+      console.error(err);
+    });
+    if (id) navigate(`draft/${id}/edit`);
+  }
+
   return (
     <div className={styles.main}>
-      <button>Create a new blog</button>
+      <button onClick={handleClick}>Create a new blog</button>
     </div>
   )
 } 
