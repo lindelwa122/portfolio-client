62f4232ed456e9f447072d5fe0a33eda381a3554
Lindelwa <lindelwa.codes@gmail.com>
2024-03-14 15:10:40 +0200

    Update the url.
    I updated the url from 'create-draft' to 'blog/create-draft' because
    I had forgotten to prefix it with 'blog'.

--- src/utils/send-data-to-server.jsx
+++ src/utils/send-data-to-server.jsx
@@ -1,7 +1,7 @@
 import { serverURI } from './global-variables';
 
 const createDraft = async () => {
-  const response = await fetch(serverURI + '/create-draft', { method: 'POST' });
+  const response = await fetch(serverURI + '/blog/create-draft', { method: 'POST' });
   if (!response.ok) {
     const err = new Error(
       `There was an error. Status returned is ${response.status}.`
