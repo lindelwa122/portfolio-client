99f19e7a7e7ef197e4015a55b53a7b4ef2d1ceea
Lindelwa <lindelwa.codes@gmail.com>
2024-03-16 15:03:46 +0200

    Add a hook to get drafts.

--- src/utils/custom-hooks.jsx
+++ src/utils/custom-hooks.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useState, useRef } from 'react';
+import { serverURI } from './global-variables';
 
 const useInterval = (callback, delay) => {
   const savedCallback = useRef();
@@ -19,4 +20,30 @@ const useInterval = (callback, delay) => {
   }, [delay]);
 }
 
-export { useInterval };
+const useDraftsData = () => {
+  const [drafts, setDrafts] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  useEffect(() => {
+    try {
+      const response = fetch(serverURI + '/blog/drafts');
+      if (!response.ok) {
+        const err = new Error(`Something wrong occurred. Status: ${response.status}.`);
+        err.status = response.status;
+        throw err;
+      }
+
+      const { drafts } = response.json();
+      setDrafts(drafts);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  return { drafts, error, loading };
+}
+
+export { useInterval, useDraftsData };
