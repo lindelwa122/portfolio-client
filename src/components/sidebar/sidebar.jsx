ccd32088b9a7cfdcc93e3cefaf5747f271f38726
Lindelwa <lindelwa.codes@gmail.com>
2024-03-16 16:53:15 +0200

    Add and list drafts.

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,7 +1,27 @@
+import { useNavigate } from 'react-router-dom';
+import { nanoid } from 'nanoid';
 import styles from './sidebar.module.css';
+import { useDraftsData } from '../../utils/custom-hooks';
 
 const Sidebar = () => {
-  return <div className={styles.sidebar}></div>
+  const { error, loading, drafts } = useDraftsData();
+  const navigate = useNavigate();
+
+  if (error) return <div>There was an error.</div>
+
+  if (loading) return <div>Still loading....</div>
+
+  const draftList = drafts.map(draft => (
+    <div 
+      key={nanoid()}
+      onClick={() => {
+        navigate(`/blogs/create/draft/${draft.id}/edit`)
+      }}>
+        {draft.title}
+    </div>
+  ));
+
+  return <div className={styles.sidebar}>{draftList}</div>
 }
 
 export default Sidebar;
