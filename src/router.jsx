e8d605b29b6ab4d15fe0709a867f45eac6b9b154
Lindelwa <lindelwa.codes@gmail.com>
2024-03-21 14:32:49 +0200

    Add AllBlogs and Blogs components.

--- src/router.jsx
+++ src/router.jsx
@@ -2,7 +2,9 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import CreateBlog from './components/create_blog/create-blog';
 import CreateBlogIndex from './components/create_blog_index/create-blog-index';
+import AllBlogs from './components/all_blogs/all-blogs';
 import BlogEditor from './components/blog_editor/blog-editor';
+import Blog from './components/blog/blog';
 
 const Router = () => {
   const router = createBrowserRouter([
@@ -19,7 +21,15 @@ const Router = () => {
           element: <BlogEditor />,
         }
       ]
-    }
+    },
+    {
+      path: '/blog',
+      element: <AllBlogs />,
+    },
+    {
+      path: '/blog/:id',
+      element: <Blog />,
+    },
   ]);
 
   return <RouterProvider router={router} />;
