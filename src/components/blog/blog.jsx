8b768e4ffd443903147dcb36bebefc91ea2bb33c
Lindelwa <lindelwa.codes@gmail.com>
2024-03-27 19:54:19 +0200

    Display blog from the server.

--- src/components/blog/blog.jsx
+++ src/components/blog/blog.jsx
@@ -1,24 +1,29 @@
+import { useParams } from 'react-router-dom';
+
 import Navbar from '../navbar/navbar';
 import Footer from '../footer/footer';
 import styles from './blog.module.css';
 import { banner, mobileAd, mobileAdblock } from '../all_blogs/all-blogs.module.css';
+import { useBlogData } from '../../utils/custom-hooks';
 
 const Blog = () => {
+  const { id } = useParams();
+  const { error, blog, loading } = useBlogData(id);
+
   return (
     <>
       <Navbar />
-      <main className={styles.main}>
+      {loading && <p style={{textAlign: 'center'}}>Loading...</p>}
+      {error && <p style={{textAlign: 'center'}}>An error occurred while fetching your blog. Please check your internet connection and try again later.</p>}
+      {blog && <main className={styles.main}>
         <aside className={styles.aside}>
         </aside>
         <div className={styles.blogContainer}>
-          <h1 className={styles.h1}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex, pariatur?</h1>
+          <h1 className={styles.h1}>{blog.title}</h1>
           <aside className={mobileAd}>
             <div className={mobileAdblock}></div>
           </aside>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae doloribus officiis beatae pariatur maiores nostrum, facere, impedit illum voluptates est, exercitationem assumenda quae. Dolore voluptate ab reiciendis nobis magni minima. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae doloribus officiis beatae pariatur maiores nostrum, facere, impedit illum voluptates est, exercitationem assumenda quae. Dolore voluptate ab reiciendis nobis magni minima.</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam consectetur tenetur expedita itaque sequi mollitia illo dignissimos sunt omnis repudiandae assumenda quod quo, nulla explicabo inventore dolorum totam. Nulla, numquam enim omnis et tempora dolorum voluptatum dolor nobis quis pariatur minima deleniti! Vel, officia blanditiis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam consectetur tenetur expedita itaque sequi mollitia illo dignissimos sunt omnis repudiandae assumenda quod quo, nulla explicabo inventore dolorum totam. Nulla, numquam enim omnis et tempora dolorum voluptatum dolor nobis quis pariatur minima deleniti! Vel, officia blanditiis!</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam consectetur tenetur expedita itaque sequi mollitia illo dignissimos sunt omnis repudiandae assumenda quod quo, nulla explicabo inventore dolorum totam. Nulla, numquam enim omnis et tempora dolorum voluptatum dolor nobis quis pariatur minima deleniti! Vel, officia blanditiis!</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam fuga cum nihil sunt eveniet! Consequuntur non, nam tempore molestiae ad provident saepe consectetur earum ratione.</p>
+          <div dangerouslySetInnerHTML={{ __html: blog.content }}></div>
         </div>
         <aside className={mobileAd}>
             <div className={mobileAdblock}></div>
@@ -26,7 +31,7 @@ const Blog = () => {
         <aside className={styles.aside}>
           <div className={banner}></div>
         </aside>
-      </main>
+      </main>}
       <Footer />
     </>
   )
